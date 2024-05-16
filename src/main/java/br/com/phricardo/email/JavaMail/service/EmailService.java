0a91c616850af950adca71d14ef5d76f9e65e060
Pedro Ricardo <brpedroricardo@gmail.com>
2024-05-15 21:50:58 -0300

    fix: add bug fix

--- src/main/java/br/com/phricardo/email/JavaMail/service/EmailService.java
+++ src/main/java/br/com/phricardo/email/JavaMail/service/EmailService.java
@@ -1,24 +1,28 @@
 package br.com.phricardo.email.JavaMail.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class EmailService {
 
-    private JavaMailSender javaMailSender;
+    private final JavaMailSender javaMailSender;
 
-    public EmailService(JavaMailSender javaMailSender) {
-        this.javaMailSender = javaMailSender;
-    }
+    @Value("${spring.mail.properties.mail.from}")
+    public String from;
 
-    public void sendEmail(String to, String subject, String text) {
+    public void sendEmail(final String to, final String subject, final String text) {
+        val msg = new SimpleMailMessage();
 
-        SimpleMailMessage msg = new SimpleMailMessage();
         msg.setTo(to);
         msg.setSubject(subject);
         msg.setText(text);
+        msg.setFrom(from);
 
         javaMailSender.send(msg);
     }
