1a2554928415a0972302e10fda98643cf41ae403
Pedro Ricardo <brpedroricardo@gmail.com>
2025-07-05 01:24:49 -0300

    feat(config): add ThreadPoolConfig to customize async task executor

--- src/main/java/com/phricardo/email/JavaMail/JavaMailApplication.java
+++ src/main/java/com/phricardo/email/JavaMail/JavaMailApplication.java
@@ -2,9 +2,7 @@ package com.phricardo.email.JavaMail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableAsync
 @SpringBootApplication
 public class JavaMailApplication {
 
---
+++ src/main/java/com/phricardo/email/JavaMail/configuration/ThreadPoolConfig.java
@@ -0,0 +1,10 @@
+package com.phricardo.email.JavaMail.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@Configuration
+public class ThreadPoolConfig {
+
+}
